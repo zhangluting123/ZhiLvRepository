681429a6b060c3ba301d28ecf4b32b71d592774d
zhangmr1 <zmr_fairy@163.com>
2021-01-29 16:21:36 +0800

    add recommendUser1

---
+++ server/ZhiLvProject/src/com/zhilv/recommendUser/controller/RecommendUserController.java
@@ -0,0 +1,11 @@
+package com.zhilv.recommendUser.controller;
+
+/**
+ * @ClassName:RecommendUserController
+ * @description:TODO
+ * @author 张梦如
+ * @date:2021年1月29日
+ */
+public class RecommendUserController {
+
+}
